fb19705cf2a5ec17a27a6bc9fc9b60e4396619a5
thibault <thibault.odor@outlook.com>
2023-02-03 08:10:18 +0100

    Simple square

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -40,10 +40,174 @@ float lastFrame = 0.0f;
 //rotation
 float angle = 0.;
 float zoom = 1.;
+
+
+struct Plan {
+
+    GLuint vertexbuffer, colorbuffer, texture, uvbuffer,elementbuffer;
+
+    std::vector<vec3> liste_points;
+    std::vector<GLuint> indice_points;
+
+    int n;
+
+    void setSommet(vec3 pt1,vec3 pt2,vec3 pt4,int n) {
+        Plan::n = n;
+        float distance_dot_I = abs(pt1[0]-pt2[0])/(float)n;
+        float distance_dot_J = abs(pt1[1]-pt4[1])/(float)n;
+        for (int j = 0; j <= n; ++j) {
+            for (int i = 0; i <= n; ++i) {
+                std::cout << pt1[1]+((float)j*distance_dot_J) << " " << pt1[1] << std::endl;
+                liste_points.emplace_back(pt1[0]+((float)i*distance_dot_I),pt1[1]-((float)j*distance_dot_J),pt1[2]);
+            }
+        }
+    }
+
+    void initBuffers() {
+        for (int j = 0; j < (n-1); ++j) {
+            for (int i = 0; i < (n-1); ++i) {
+                indice_points.emplace_back(i+(j*(n+1)));
+                indice_points.emplace_back(i+(j*(n+1))+1);
+                indice_points.emplace_back(i+((j+1)*(n+1)));
+
+                indice_points.emplace_back(i+((j+1)*(n+1))+1);
+                indice_points.emplace_back(i+((j+1)*(n+1)));
+                indice_points.emplace_back(i+(j*(n+1))+1);
+            }
+        }
+
+        // Couleur des points
+        static const GLfloat g_color_buffer_data[] = {
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f
+        };
+
+        // Position des UVs
+        static const GLfloat g_uv_buffer_data[] = {
+                0.0f, 1.0f,
+                1.0f, 1.0f,
+                1.0f, 0.0f,
+                0.0f, 1.0f,
+                0.0f, 0.0f,
+                1.0f, 0.0f
+        };
+
+        // OpenGL nous aide à cette tâche avec ses "Buffers" (ou "tampons" en français, qu'il faut imaginer comme des vrais tampons à encre) :
+        // on créer un tampon (glGenBuffers), on le prend en main (glBindBuffer), et on y inscrit les tableaux avec de l'encre du côté CPU qu'OpenGL
+        // recolle au GPU (glBufferData) dans l'ordre que l'on souhaite (glVertexAttribPointer).
+
+        // Creer un premier buffer contenant les positions
+        // a mettre dans le layout 0
+
+        // Buffer généré a stocker
+        glGenBuffers(1, &vertexbuffer);
+        // On prévient OPENGL que l'on va utiliser ce buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        // Informations que l'on veut envoyer au GPU
+        glBufferData(GL_ARRAY_BUFFER, liste_points.size() * sizeof(glm::vec3), &liste_points[0], GL_STATIC_DRAW);
+
+        glGenBuffers(1, &elementbuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,elementbuffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,indice_points.size()*sizeof(unsigned int),indice_points.data(),GL_STATIC_DRAW);
+
+        /*
+        // Creer un deuxieme buffer contenant les couleurs
+        // a mettre dans le layout 1
+        glGenBuffers(1, &colorbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        // Informations que l'on veut envoyer au GPU
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+        // Creer un troisieme buffer UV
+        // a mettre dans le layout 1
+        glGenBuffers(1, &uvbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        // Informations que l'on veut envoyer au GPU
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+        */
+
+        /*
+        glGenTextures(1,&texture);
+        glBindTexture(GL_TEXTURE_2D,texture);
+        // load and generate the texture
+        int width,height,nrChannels;
+        unsigned char * data = stbi_load( "pic.png" , &width , &height , &nrChannels , 0);
+        if (data){
+            glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else{
+            std::cout << " Failed to load texture " << std::endl ;
+        }
+        stbi_image_free(data);
+
+        // GLU dependencies
+        glBindTexture(GL_TEXTURE_2D,texture);
+        gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGB,width,height,GL_RGB,GL_UNSIGNED_BYTE,data);
+
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        */
+    }
+    void clearBuffers(){
+        //Liberer la memoire, utiliser glDeleteBuffers
+        glDeleteBuffers(1,&vertexbuffer);
+        glDeleteBuffers(1,&colorbuffer);
+        glDeleteBuffers(1,&uvbuffer);
+        glDeleteTextures(1,&texture);
+    }
+    void draw (){
+        // 1rst attribute buffer : vertices
+        //A faire
+        //Utiliser glVertexAttribPointer
+        // Vertex = Layout 0 (premier attribut)
+        // On bind le triangle vertex (dernier bind = celui qui sera draw)
+        glBindBuffer(GL_ARRAY_BUFFER,vertexbuffer);
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 3 * sizeof(float), (void*)0);
+
+        //Ajouter un attribut dans un color buffer à envoyé au GPU
+        //Utiliser glVertexAttribPointer
+        // 2nd attribute buffer : normals
+        //glBindBuffer(GL_ARRAY_BUFFER,colorbuffer);
+        //glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE, 3 * sizeof(float), (void*)0);
+
+        //glBindBuffer(GL_ARRAY_BUFFER,uvbuffer);
+        //glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE, 2 * sizeof(float), (void*)0);
+
+        /*
+        // activate usage of 2 D textures
+        glEnable(GL_TEXTURE_2D);
+        // activate texture ( limited to 32 or less depending on machine )
+        int indexActiveTexture = 0; // 0 to 31
+        // bind texture as Texture 0
+        glBindTexture(GL_TEXTURE_2D , texture);
+        glActiveTexture(GL_TEXTURE0 + indexActiveTexture);
+        // set used active texture ( Modern OpenGL )
+        glUniform1i(glGetUniformLocation(programID,"texture1"),indexActiveTexture) ;
+         */
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,elementbuffer);
+        glDrawElements(GL_TRIANGLES, indice_points.size(), GL_UNSIGNED_INT, (void*)0);
+
+        //Pensez à desactive les AttributArray
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glDisableVertexAttribArray(2);
+    }
+};
 /*******************************************************************************/
 
 int main( void )
 {
+    Plan p;
     // Initialise GLFW
     if( !glfwInit() )
     {
@@ -112,9 +276,17 @@ int main( void )
     std::vector<std::vector<unsigned short> > triangles;
     std::vector<glm::vec3> indexed_vertices;
 
+
+
+    //
+    int n = 4;
+    p.setSommet(vec3(-0.5f,0.5f,0.0f),vec3(0.5f,0.5f,0.0f),vec3(-0.5f,-0.5f,0.0f),n);
+    p.initBuffers();
+    //
+
     //Chargement du fichier de maillage
-    std::string filename("chair.off");
-    loadOFF(filename, indexed_vertices, indices, triangles );
+    //std::string filename("chair.off");
+    //loadOFF(filename, indexed_vertices, indices, triangles );
 
     // Load it into a VBO
 
@@ -161,44 +333,28 @@ int main( void )
 
 
         /*****************TODO***********************/
+        mat4 model_matrix;
+        mat4 projection_matrix;
+        mat4 view_matrix;
+
         // Model matrix : an identity matrix (model will be at the origin) then change
+        int id_model_matrix = glGetUniformLocation(programID,"model_matrix");
+        glUniformMatrix4fv(id_model_matrix,1,GL_FALSE,&model_matrix[0][0]);
 
         // View matrix : camera/view transformation lookat() utiliser camera_position camera_target camera_up
+        view_matrix = glm::lookAt(camera_position,camera_target,camera_up);
+        int id_view_matrix = glGetUniformLocation(programID,"view_matrix");
+        glUniformMatrix4fv(id_view_matrix,1,GL_FALSE,&view_matrix[0][0]);
 
         // Projection matrix : 45 Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-
-        // Send our transformation to the currently bound shader,
-        // in the "Model View Projection" to the shader uniforms
-
+        projection_matrix = glm::perspective(45.0f, 1.33f, 0.1f, 100.0f);
+        int id_projection_matrix = glGetUniformLocation(programID,"proj_matrix");
+        glUniformMatrix4fv(id_projection_matrix,1,GL_FALSE,&projection_matrix[0][0]);
         /****************************************/
 
-
-
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-                    0,                  // attribute
-                    3,                  // size
-                    GL_FLOAT,           // type
-                    GL_FALSE,           // normalized?
-                    0,                  // stride
-                    (void*)0            // array buffer offset
-                    );
-
-        // Index buffer
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-
-        // Draw the triangles !
-        glDrawElements(
-                    GL_TRIANGLES,      // mode
-                    indices.size(),    // count
-                    GL_UNSIGNED_SHORT,   // type
-                    (void*)0           // element array buffer offset
-                    );
-
-        glDisableVertexAttribArray(0);
+        //
+        p.draw();
+        //
 
         // Swap buffers
         glfwSwapBuffers(window);
@@ -207,6 +363,10 @@ int main( void )
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0 );
+
+    //
+    p.clearBuffers();
+    //
 
     // Cleanup VBO and shader
     glDeleteBuffers(1, &vertexbuffer);
@@ -234,7 +394,18 @@ void processInput(GLFWwindow *window)
         camera_position += cameraSpeed * camera_target;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
         camera_position -= cameraSpeed * camera_target;
-
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+        camera_position[0] -= cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+        camera_position[0] += cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+        camera_position[1] -= cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+        camera_position[1] += cameraSpeed;
+    }
     //TODO add translations
 
 }
